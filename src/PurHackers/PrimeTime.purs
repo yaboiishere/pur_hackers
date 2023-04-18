dc2bafda6a1759ed6d5be34865a94453381b6571
Mihail Dobrev <mixaildobrev@gmail.com>
2023-04-18 20:53:59 +0300

    Initial commit of the prime time exercise

--- src/PurHackers/PrimeTime.purs
+++ src/PurHackers/PrimeTime.purs
@@ -1,16 +1,19 @@
-module PurHackers.TcpEchoServer where
+module PurHackers.PrimeTime where
 
 import Prelude
 
 import Data.Either (Either(..))
+import Data.Foldable (traverse_)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
+import Data.String (Pattern(..))
+import Data.String as String
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Erl.Atom (atom)
 import Erl.Data.Binary (Binary)
-import Erl.Data.Binary.IOData (fromBinary)
+import Erl.Data.Binary.IOData (IOData, concat, fromBinary, fromString)
 import Erl.Data.List (List, fromFoldable)
 import Erl.Kernel.Inet (ActiveError(..), ConnectedSocket, PassiveSocket, Port(..))
 import Erl.Kernel.Tcp (TcpSocket, listenPassive)
@@ -18,16 +21,18 @@ import Erl.Kernel.Tcp as Tcp
 import Erl.Process.Raw (spawn)
 import Erl.Types (Timeout(..))
 import Foreign as Foreign
-import Pinto (RegistryName(..), StartLinkResult)
-import Pinto.GenServer (InfoFn, InitResult(..), ServerPid, ServerType)
+import Pinto (RegistryName(..))
+import Pinto.GenServer (InitResult(..), ServerPid, ServerType, InfoFn)
 import Pinto.GenServer as GenServer
 import Pinto.Timer as Timer
+import Pinto.Types (RegistryName, StartLinkResult)
 import PurHackers.Logger (logInfo)
-import PurHackers.TcpEchoServer.Types (Messages(..), State)
+import PurHackers.PrimeTime.Types (Messages(..), State)
 import PurHackers.Types (TcpPort(..))
+import Unsafe.Coerce as UnsafeCoerce
 
 serverName :: RegistryName (ServerType Unit Unit Messages State)
-serverName = Local $ atom "echo_server"
+serverName = Local $ atom "prime_time"
 
 startLink :: TcpPort -> Effect (StartLinkResult (ServerPid Unit Unit Messages State))
 startLink port = GenServer.startLink (GenServer.defaultSpec $ init port)
@@ -62,22 +67,58 @@ handleInfo Accept state = do
       _ <- liftEffect $ logInfo "Error accepting connection" { message: "Error accepting connection", data: err }
       pure $ GenServer.return state
 
-receiveSocket :: TcpSocket PassiveSocket ConnectedSocket -> Effect (Unit)
+receiveSocket :: TcpSocket PassiveSocket ConnectedSocket -> Effect Unit
 receiveSocket socket = do
-  _maybeListBs <- doRecv socket $ fromFoldable []
+  _maybeIOData <- doRecv socket $ fromFoldable []
   pure unit
 
-doRecv :: TcpSocket PassiveSocket ConnectedSocket -> List Binary -> Effect (Maybe (List Binary))
+-- case maybeIOData of
+--   Just ioData ->
+--     ioData
+--       # parseMessage
+--       # (\x -> doMessages x socket)
+--
+--   Nothing ->
+--     pure unit
+
+doMessages :: Array String -> TcpSocket PassiveSocket ConnectedSocket -> Effect Unit
+doMessages messages socket =
+  traverse_ (\m -> doMessage m socket) messages
+
+doMessage :: String -> TcpSocket PassiveSocket ConnectedSocket -> Effect Unit
+doMessage message socket =
+  message
+    # encodeMessage
+    # Tcp.send socket
+    >>=
+      ( \x -> case x of
+          Right _ -> pure unit
+          Left err -> do
+            _ <- logInfo "Error sending message" { message: "Error sending message", data: err } # liftEffect
+            _ <- Tcp.close socket # liftEffect
+            pure unit
+      )
+
+doRecv :: TcpSocket PassiveSocket ConnectedSocket -> List Binary -> Effect (Maybe (IOData))
 doRecv socket bs = do
   received <- Tcp.recv socket 0 (Timeout $ Milliseconds 600000.0)
   case received of
     Right receivedBinary -> do
-      _ <- liftEffect $ Tcp.send socket $ fromBinary receivedBinary
+      _ <- (receivedBinary # fromBinary # parseMessage # (\x -> doMessages x socket))
       doRecv socket $ bs <> fromFoldable [ receivedBinary ]
     Left ActiveClosed -> do
       _ <- logInfo "Connection closed" { message: "Connection closed" } # liftEffect
-      pure $ Just bs
+      pure $ Just $ listBinaryToIOData bs
     Left err -> do
       _ <- logInfo "Error receiving message" { message: "Error receiving messages", data: err } # liftEffect
       _ <- Tcp.close socket # liftEffect
       pure $ Nothing
+
+listBinaryToIOData :: List Binary -> IOData
+listBinaryToIOData = concat <<< map fromBinary
+
+parseMessage :: IOData -> Array String
+parseMessage rawData = String.split (Pattern "\n") $ UnsafeCoerce.unsafeCoerce rawData
+
+encodeMessage :: String -> IOData
+encodeMessage = fromString
---
+++ src/PurHackers/PrimeTime/Types.purs
@@ -0,0 +1,24 @@
+module PurHackers.PrimeTime.Types where
+
+import Data.Eq (class Eq)
+import Data.Generic.Rep (class Generic)
+import Data.Show (class Show)
+import Data.Show.Generic (genericShow)
+import Erl.Kernel.Inet (ListenSocket, PassiveSocket, Port)
+import Erl.Kernel.Tcp (TcpSocket)
+
+data Messages = Accept
+
+derive instance eqMessages :: Eq Messages
+derive instance genericMessages :: Generic Messages _
+
+instance showMessages :: Show Messages where
+  show = genericShow
+
+type State =
+  { port :: Port
+  , connection :: TcpSocket PassiveSocket ListenSocket
+  }
+
+type Message =
+  { method :: String, number :: Number }
